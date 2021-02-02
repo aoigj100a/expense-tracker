73f09830a24267165a66e362d44ba36b3fc26b9f
aoigj100a <aoigj100@gmail.com>
2021-02-02 17:37:25 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+// const bodyParser = require('body-parser')
+// const methodOverride = require('method-override')
+
+const app = express()
+const port = 3000
+
+//啟動樣版引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+//安置靜態資源
+app.use(express.static('public'))
+
+//取得db連線
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+    console.log('mongoDB error!')
+})
+db.once('open', () => {
+    console.log('mongoDB connected!')
+})
+
+app.get('/', (req, res) => {
+    // res.send('專案初始化1')
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`已經連線到http://localhost:${port}`)
+})
