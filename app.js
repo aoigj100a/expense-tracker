87a9ec3b0bfb6bda4fa9a9b8bb05641606783be5
aoigj100a <aoigj100@gmail.com>
2021-02-05 16:03:08 +0800

    建立views與路由

--- app.js
+++ app.js
@@ -24,9 +24,22 @@ db.once('open', () => {
     console.log('mongoDB connected!')
 })
 
+//路由設定
 app.get('/', (req, res) => {
     // res.send('專案初始化1')
     res.render('index')
+})
+
+app.get('/new', (req, res) => {
+    res.render('new')
+})
+
+app.get('/edit', (req, res) => {
+    res.render('edit')
+})
+
+app.get('/delete', (req, res) => {
+    res.render('delete')
 })
 
 app.listen(port, () => {
