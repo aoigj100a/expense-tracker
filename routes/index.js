6098fc2d186367ff24a7460a3ab2623b06773b6b
aoigj100a <aoigj100@gmail.com>
2021-03-18 14:43:06 +0800

    feat:使用者驗證

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router =  express.Router()
 const index = require('./modules/index')
 const record = require('./modules/record')
 const user = require('./modules/user')
+const { authenticator } = require('../middleware/auth') 
 
-router.use('/', index)
-router.use('/record', record)
 router.use('/user', user)
+router.use('/record', authenticator, record)
+router.use('/', authenticator, index)
 
 module.exports = router
