b1589dc49acfc96323f17456d8a643f137ea7655
aoigj100a <aoigj100@gmail.com>
2021-03-19 13:59:23 +0800

    feat:加入加入fb登入

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ const router =  express.Router()
 const index = require('./modules/index')
 const record = require('./modules/record')
 const user = require('./modules/user')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth') 
 
 router.use('/user', user)
+router.use('/auth', auth)
 router.use('/record', authenticator, record)
 router.use('/', authenticator, index)
 
