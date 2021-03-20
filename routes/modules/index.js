8ef5bef4a2a034865669e277d3c90d21315f8030
aoigj100a <aoigj100@gmail.com>
2021-03-20 16:09:18 +0800

    modify:重構首頁路由

--- routes/modules/index.js
+++ routes/modules/index.js
@@ -7,27 +7,18 @@ const Category = require('../../models/category')
 //首頁
 // totalAmount 不能輸入負值
 router.get('/', async (req, res) => {
-    const categoryList = await Category.find().lean().exec()
     const userId = req.user._id
-    Record.find({ userId }).lean()
-        .then(records => {
-            let totalAmount = 0
-            let category = ""
-            records.forEach(record => {
-                totalAmount += record.amount
-                categoryList.forEach(
-                    (categories) => {
-                        if (categories.cName == record.category) {
-                            category = categories.cIconI
-                            record.icon = category
-                            return category
-                        }
-                    }
-                )
-            })
-            res.render('index', { records, totalAmount, userId })
-        }).catch(error => console.log(error))
+    const categoryList = await Category.find().lean().exec()
+    const records = await Record.find({ userId }).lean().exec()
+    let totalAmount = 0
 
+    records.forEach(record => {
+        totalAmount += record.amount
+        const category = categoryList.find(category => category.cName === record.category)
+        record.icon = category.cIconI
+    })
+    res.render('index', { records, totalAmount, userId })
+        .catch(error => console.log(error))
 })
 
 module.exports = router
